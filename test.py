4530d293fddbca63621325277a1221b801098a29
eelod9 <eelodgnod@gmail.com>
2022-11-29 07:28:57 -0800

    request

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import math
+import requests
+
 
 print("hello")
 #testing
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
