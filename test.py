fcb58892c808cf4f74c542ca0a3b979bb404e48e
eelod9 <eelodgnod@gmail.com>
2022-11-30 07:38:46 -0800

    Soup

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import math
 import requests
-
+from BeautifulSoup import BeautifulSoup
 
 print("hello")
 #testing
 def request(url):
     try:
-        return requests.get("http://" + url)
+        return requests.get(url)
     except requests.exceptions.ConnectionError:
         pass
+
+target_url = "google.com"
+response = request(target_url)
+print(response.content)
+    
