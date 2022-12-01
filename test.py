7834f32cb0c278c8917b4500b79940e4f568738e
eelod9 <eelodgnod@gmail.com>
2022-12-01 09:08:18 -0800

    hello

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-from bs4 import BeautifulSoup as bs
-import requests
-url = "https://www.stackoverflow.com/"
-
-html_content = requests.get(url).text
-soup = bs(html_content, "html.parser")
-
-if soup.find('title').text is not None:
-  print(soup.find('title').text)
+hello
