1986150b5e874196517c79f7f41ad9dedbcaa9d2
eelod9 <eelodgnod@gmail.com>
2022-12-15 07:32:36 -0800

    hangman start

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-hello
+nums= [2,7,11,15]
+target = 9
+f = {}
+for i, num in enumerate(nums):
+    if num not in f:
+        f[target-num]= i
+        print(f"Update{f}")
+    else:
+        f[num] , i
+        print(f"{f[num]}")
+        print(i)
+        
