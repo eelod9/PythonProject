faeef9d39a2988e3e7240ce8cc15bbd4f4ffd575
eelod9 <eelodgnod@gmail.com>
2022-12-29 10:34:11 -0800

    hello

---
+++ caesarCipher.py
@@ -0,0 +1,82 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
+'t', 'u', 'v', 'w', 'x', 'y', 'z']
+'''
+def encrypt(word, num):
+    newText = ""
+    encrpytedChar =""
+    lengthArray = len(alphabet)
+    for char in word:
+        position = alphabet.index(char)
+        newPosition = position + num
+        print(f"what is newPosition {newPosition}")
+        #print(f"what is the length {lengthArray}")
+        
+        if newPosition > lengthArray:
+            shiftPosition = newPosition - lengthArray
+            #print(f"what is the shiftPosition {shiftPosition}")
+            encrpytedChar = alphabet[shiftPosition]
+        else:
+            encrpytedChar = alphabet[newPosition]
+        newText += encrpytedChar
+    
+    print(f"Your message has been encrypted => {newText}")
+    
+def decrypt(word, num):
+    newText = ""
+    encrpytedChar =""
+    lengthArray = len(alphabet)
+    for char in word:
+        position = alphabet.index(char)
+        newPosition = position - num
+        #print(f"what is newPosition {newPosition}")
+        #print(f"what is the length {lengthArray}")
+        
+        if newPosition > lengthArray:
+            shiftPosition = newPosition + lengthArray
+            #print(f"what is the shiftPosition {shiftPosition}")
+            encrpytedChar = alphabet[shiftPosition]
+        else:
+            encrpytedChar = alphabet[newPosition]
+        newText += encrpytedChar
+    
+    print(f"Your message has been encrypted => {newText}")
+'''
+def caesar(word,num,operation):
+    newText = ""
+    encrpytedChar =""
+    lengthArray = len(alphabet)
+    if operation == 'encode':
+        for char in word:
+            position = alphabet.index(char)
+            newPosition = position + num
+            print(f"what is newPosition {newPosition}")
+            #print(f"what is the length {lengthArray}")
+            
+            if newPosition > lengthArray:
+                shiftPosition = newPosition - lengthArray
+                #print(f"what is the shiftPosition {shiftPosition}")
+                encrpytedChar = alphabet[shiftPosition]
+            else:
+                encrpytedChar = alphabet[newPosition]
+            newText += encrpytedChar
+    else:
+        for char in word:
+            position = alphabet.index(char)
+            newPosition = position - num
+            #print(f"what is newPosition {newPosition}")
+            #print(f"what is the length {lengthArray}")
+            
+            if newPosition > lengthArray:
+                shiftPosition = newPosition + lengthArray
+                #print(f"what is the shiftPosition {shiftPosition}")
+                encrpytedChar = alphabet[shiftPosition]
+            else:
+                encrpytedChar = alphabet[newPosition]
+            newText += encrpytedChar
+    print(f"Your message has been encrypted => {newText}")
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+caesar(text,shift,direction)
