fd12b3076cff7c62ef4e5492289eb6175b81c5da
eelod9 <eelodgnod@gmail.com>
2023-01-17 11:37:52 -0800

    finished

--- auctionProgram.py
+++ auctionProgram.py
@@ -1,4 +1,4 @@
-from replit import clear
+import os
 logo = '''
                          ___________
                          \         /
@@ -14,15 +14,31 @@ logo = '''
 '''
 def checkAnswer(ans):
   if(ans == 'yes'):
+    print(chr(27) + "[2J")
     return True
-  else(ans == 'no'):
+  elif(ans == 'no'):
     return False
-
+bidDict = {}
 contin = True
 print(logo)
 while(contin):
-  
   name = input("What's your name?: ")
   bid = input("What's your bid?: ")
+  bidDict[name] = int(bid)
+  print(bidDict)
   ans = input("Are there other users to bid?: ")
   contin = checkAnswer(ans)
+
+greatestNum = 0
+
+for key in bidDict:
+  value = bidDict[key]
+  if (greatestNum < value):
+    greatestNum = value
+    greatestName = key
+
+
+print(f"{greatestName} is the highest bidder!")
+
+
+
