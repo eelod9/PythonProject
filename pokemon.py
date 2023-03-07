07b535a27f35fa0d23dde4ac68a70baee918a79f
Ella <eelodgnod@gmail.com>
2023-03-07 08:07:04 -0800

    start

---
+++ pokemon.py
@@ -0,0 +1,8 @@
+from prettytable import PrettyTable
+
+x = PrettyTable() 
+x.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+x.add_column("Type",["Electric","Water","Fire"])
+x.align = "l"
+print(x)
+print("hello")
