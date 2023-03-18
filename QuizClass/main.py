894e1b440d8caa032ec3fc6f829566f4198689cf
Ella <eelodgnod@gmail.com>
2023-03-18 10:58:52 -0700

    done

--- QuizClass/main.py
+++ QuizClass/main.py
@@ -1,13 +1,23 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 questionBank = []
 
 for key, value in enumerate(question_data):
     text = value["text"]
     answer = value["answer"]
-    print(text)
+    
     new_question = Question(text, answer)
     questionBank.append(new_question)
 
-print(questionBank)
+print(questionBank[0].text)
+
+    
+new_quizBrain = QuizBrain(questionBank)
+
+while(new_quizBrain.still_has_questions()):
+    new_quizBrain.next_quesiton()
+
+
+
--- QuizClass/quiz_brain.py
+++ QuizClass/quiz_brain.py
@@ -2,7 +2,22 @@ class QuizBrain:
     def __init__(self,question_list):
         self.questNum = 0
         self.questList = question_list
+        self.score = 0
+    
+    def still_has_questions(self):
+        return self.questNum < len(self.questList)
+        
 
     def next_quesiton(self):
         current_question = self.questList[self.questNum]
-        input(f"{current_question.text}")
+        self.questNum += 1 
+        user_answer = input(f"Q.{self.questNum}: {current_question.text} (True or False):")
+        self.check_answer(user_answer, current_question.answer)
+    
+    def check_answer(self, user_answer, correct_answer):
+        if(user_answer.lower() == correct_answer.lower()):
+            self.score += 1
+            print(f"correct! current score: {self.score}")
+        else:
+            print(f"Incorrect current score: {self.score} ")
+    
