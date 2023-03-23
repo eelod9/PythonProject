2e1d177ae2d725b8273b990ce45adb7af6549b0f
Ella <eelodgnod@gmail.com>
2023-03-23 09:46:16 -0700

    turtle package

---
+++ Turtle/main.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("#FFB6C1")
+
+
+for _ in range(4):
+    timmy.right(90)
+    timmy.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
